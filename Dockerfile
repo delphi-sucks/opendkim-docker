8beaf02cab2e9ff643892736fb298a87110ce87a
Sebastian Hellwig <sebastian@furufuru.de>
2022-03-22 20:35:28 +0100

    Log & Config

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ FROM alpine:latest
 # Install dependencies
 RUN apk add --no-cache --update \
     opendkim opendkim-utils \
-    bash
+    bash syslog-ng
 
 VOLUME [ "/etc/dkimkeys" ]
 
 EXPOSE 8891/tcp
 
-COPY entrypoint.sh /usr/local/bin/
+COPY files /
 RUN chmod 0755 /usr/local/bin/entrypoint.sh
 
 ENTRYPOINT ["entrypoint.sh"]
---
+++ files/usr/local/bin/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -e
+
+CONFIG_PATH=/etc/opendkim/opendkim.conf
+KEY_PATH=/etc/dkimkeys
+
+function setConfig() {
+    sed -i -e 's|$1 .*||' $CONFIG_PATH
+    echo "$1 $2" >> $CONFIG_PATH
+}
+
+# Create DKIM key
+if [ ! -f $KEY_PATH/$OPENDKIM_SELECTOR.key ]
+then
+    echo "Create DKIM key..."
+    opendkim-genkey -s $OPENDKIM_SELECTOR -d "$DOMAIN" -D $KEY_PATH
+    mv $KEY_PATH/$OPENDKIM_SELECTOR.private $KEY_PATH/$OPENDKIM_SELECTOR.key
+    echo "Key created. Add the following TXT record to your DNS:"
+    cat $KEY_PATH/$OPENDKIM_SELECTOR.txt
+fi
+
+chown -R opendkim:opendkim $KEY_PATH
+chmod 0750 $KEY_PATH
+
+# Config
+setConfig "Domain" "${OPENDKIM_DOMAIN}"
+setConfig "Selector" "${OPENDKIM_SELECTOR}"
+setConfig "KeyFile" "/etc/dkimkeys/${OPENDKIM_SELECTOR}.key"
+setConfig "ReportAddress" "postmaster@${OPENDKIM_DOMAIN}"
+setConfig "UserID" "opendkim"
+setConfig "Socket" "inet:8891@localhost"
+
+
+chmod 700 /etc/dkimkeys
+chown -R opendkim:opendkim /etc/dkimkeys
+
+mkdir /run/opendkim
+chown opendkim:opendkim /run/opendkim
+
+echo "Starting opendkim..."
+exec "$@"
